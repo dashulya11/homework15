df13e3dd7670d74948da108082c81d005ad19688
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2021-11-16 21:47:33 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, -1, -2, -3];
 function positiveNumbers(arr) {
     const doubleArr = [];
-    if (arr.length === 0) return console.log('Массив пустой!');
+    if (arr.length === 0) return `Массив пустой!`;
     for (let i = 0; i < arr.length; i++) {
         if (check(arr[i]) && arr[i] > 0) {
             doubleArr.push(arr[i]);
